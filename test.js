0da82069d816d8f15f7f8fc1b5dd51a23d57935c
Satyam Jha <k.satyamjha@gmail.com>
2024-11-18 12:37:28 +0530

    first commit

---
+++ test.js
@@ -0,0 +1,18 @@
+// const greeting = "Hello"
+
+// for (const greet of greeting) {
+//     console.log(`Each char is ${greeting}`)
+
+
+// }
+
+
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const a of arr) {
+    console.log(`elements are ${a}`);
+
+
+}
+
